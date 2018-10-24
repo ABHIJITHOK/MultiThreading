de770651738e50b7dd341636136456b6083115c1
Abhy.Kizhakkepat <Abhy.Kizhakkepat@tattsgroup.com>
2018-10-24 17:34:34 +1000

    Added example of cancelling threads cooperatively.

--- MultiThreading/Example4CancellingThreadsCooperatively/Program.cs
+++ MultiThreading/Example4CancellingThreadsCooperatively/Program.cs
@@ -3,13 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace Example4CancellingThreadsCooperatively
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            CancellationTokenSource cts = new CancellationTokenSource();
+
+            Console.WriteLine("Press 'C' to terminate the application.");
+
+            Thread t1 = new Thread(() =>
+                {
+                    if (Console.ReadKey(true).KeyChar.ToString().ToUpperInvariant() == "C")
+                    {
+                        cts.Cancel();
+                    }
+                }
+            );
+
+            Thread t2 = new Thread(new ParameterizedThreadStart(Server.ServerMethod));
+            t1.Start();
+            t2.Start(cts.Token);
+
+            Console.WriteLine($"Main thread with Thread Id: {Thread.CurrentThread.ManagedThreadId} is doing something.");
+            t2.Join();
+            cts.Dispose();
+            Console.WriteLine($"Main thread with Thread Id: {Thread.CurrentThread.ManagedThreadId} is done.");
         }
     }
 }
---
+++ MultiThreading/Example4CancellingThreadsCooperatively/Server.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Example4CancellingThreadsCooperatively
+{
+    public class Server
+    {
+        public static void ServerMethod(object obj)
+        {
+            CancellationToken ct = (CancellationToken)obj;
+            Console.WriteLine($"Thread Id: {Thread.CurrentThread.ManagedThreadId} : Execution of Server.ServerMethod() started.");
+            while(!ct.IsCancellationRequested)
+            {
+                //Thread.SpinWait(50000);
+                Console.WriteLine("Abhy");
+                Thread.Sleep(500);
+            }
+
+            Console.WriteLine($"Worker thread has been cancelled. Press any key to exit.");
+            Console.ReadKey(true);
+        }
+    }
+}
